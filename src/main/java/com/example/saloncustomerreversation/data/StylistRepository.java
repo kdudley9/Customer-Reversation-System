86130080f1a91ca1bec50530156ac8fedc49a39c
kdudley9 <98002234+kdudley9@users.noreply.github.com>
2023-01-08 00:39:54 -0500

    Removed classes

--- src/main/java/com/example/saloncustomerreversation/data/StylistRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.saloncustomerreversation.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StylistRepository extends JpaRepository<Stylist, Long> {
-}
